ba2e4b78168142b62e3ac8f7f8f24ddf98e17ccf
Anuj Paudel <anujpaudel98@yahoo.com>
2024-01-23 08:57:44 +0545

    location sharing enabled

--- goal/webSock/findGuide.py
+++ goal/webSock/findGuide.py
@@ -1,10 +1,14 @@
 # consumers.py
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from goal.models import Tour
+from goal.models import User,Tour
 
+#if you need to print colourful terminal output
+# from colorama import Fore, Style
+# print(f"{Fore.GREEN}{tour_id}haha{Fore.WHITE}")
 
-
+from asgiref.sync import sync_to_async
+from channels.db import database_sync_to_async
 class GuideRequestConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         # Retrieve location from the WebSocket path
@@ -38,7 +42,6 @@ class TouristConnection(AsyncWebsocketConsumer):
         #temporary getting from path
         self.user = self.scope['url_route']['kwargs']['user']
 
-        print(self.user)
         self.room_group_name = f'tour_{self.user}'
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -53,9 +56,40 @@ class TouristConnection(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
+
+    async def receive(self, text_data):
+        location_data = json.loads(text_data)
+        tour_id=location_data.get('tour_id')
+        destination = await self.get_associated_user(tour_id)
+        destination_group_name = f'tour_{destination}'
+        await self.channel_layer.group_send(
+            destination_group_name,
+            {
+                'type': 'send_location',
+                'location_data': location_data["location_data"]
+            }
+        )
     
+    async def send_location(self, location_data):
+        await self.send(text_data=json.dumps(location_data))
+        
+
     async def update(self,tour_data):
         await self.send(text_data=json.dumps(tour_data))
+    
+    @sync_to_async
+    def get_associated_user(self,tour_id):
+        try:
+
+            user=User.objects.get(id=self.user)
+            tour= Tour.objects.get(tour_id=tour_id)
+
+            if tour.tourist==user:
+                return tour.guide
+            else:
+                raise ValueError(f"Tour Mis-match")
+        except Exception as e:
+             return e
 
 class GuideConnection(AsyncWebsocketConsumer):
     
@@ -66,7 +100,6 @@ class GuideConnection(AsyncWebsocketConsumer):
         #temporary getting from path
         self.user = self.scope['url_route']['kwargs']['user']
 
-        print(self.user)
         self.room_group_name = f'tour_{self.user}'
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -81,6 +114,36 @@ class GuideConnection(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
+
+    async def receive(self, text_data):
+        location_data = json.loads(text_data)
+        tour_id=location_data.get('tour_id')
+        destination = await self.get_associated_user(tour_id)
+
+        destination_group_name = f'tour_{destination}'
+
+        await self.channel_layer.group_send(
+            destination_group_name,
+            {
+                'type': 'send_location',
+                'location_data': location_data["location_data"]
+            }
+        )
     
+    async def send_location(self, location_data):
+        await self.send(text_data=json.dumps(location_data))
+    
+    @sync_to_async
+    def get_associated_user(self,tour_id):
+        try:
+            user=User.objects.get(id=self.user)
+            tour=Tour.objects.get(tour_id=tour_id)
+            if tour.guide==user:
+                return tour.tourist
+            else:
+                raise ValueError(f"Tour Mis-match")
+        except Exception as e:
+             return e
+
     async def update(self,tour_data):
         await self.send(text_data=json.dumps(tour_data))
